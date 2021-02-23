a0897cbaf8b6571e85a942c305a5ce95bad0e15f
Keven Brousseau <65184919+Keven44977@users.noreply.github.com>
2021-02-23 09:38:05 -0500

    Creation Classe AffichageLCD

---
+++ Station Meteo/include/AffichageLCD.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+class AffichageLCD
+{
+private:
+    LiquidCrystal_I2C m_lcd = LiquidCrystal_I2C(0x00, 0, 0);
+    byte m_adresseLCD;
+    long m_tempsPrecedent;
+    char m_changerDonnees;
+
+public:
+    AffichageLCD();
+    AffichageLCD(byte p_adresse);
+    void AfficherInfos(float p_temp, float p_humidite, float p_pression, float p_altitude);
+};
---
+++ Station Meteo/src/AffichageLCD.cpp
@@ -0,0 +1,36 @@
+#include <AffichageLCD.h>
+
+AffichageLCD::AffichageLCD(byte p_adresse)
+{
+    this->m_adresseLCD = p_adresse;
+    this->m_lcd = LiquidCrystal_I2C(this->m_adresseLCD, 16, 2);
+}
+
+
+void AffichageLCD::AfficherInfos(float p_temperature, float p_humidite, float p_pression, float p_altitude)
+{
+    long temp = millis();
+
+    if (temp - this->m_tempsPrecedent > 2000)
+    {
+        this->m_tempsPrecedent = temp;
+
+        m_lcd.clear();
+
+        if (this->m_changerDonnees)
+        {
+            m_lcd.setCursor(0, 0);
+            m_lcd.print("Temperature: " + String(p_temperature) + "*C");
+            m_lcd.setCursor(0, 1);
+            m_lcd.print("Humidité: " + String(p_humidite) + "%");
+        }
+        else
+        {
+            m_lcd.setCursor(0, 0);
+            m_lcd.print("Pression: " + String(p_pression / 100) + "hPa");
+            m_lcd.setCursor(0, 1);
+            m_lcd.print("Altitude: " + String(p_pression) + "m");
+        }
+
+        this->m_changerDonnees = !this->m_changerDonnees;
+    }

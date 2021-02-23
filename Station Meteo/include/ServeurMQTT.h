b8a2da2cfe1fd37575857da586fc46050250109c
Keven Brousseau <65184919+Keven44977@users.noreply.github.com>
2021-02-23 09:38:23 -0500

    Creation Classe ServeurMQTT

---
+++ Station Meteo/include/ServeurMQTT.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <BME280.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <WiFiManager.h>
+#include <DNSServer.h>
+
+class ServeurMQTT
+{
+private:
+    WiFiManager m_wifiManager;
+    char mqtt_serveur[40];
+    char mqtt_port[5];
+    char mqtt_utilisateur[30];
+    char mqtt_motDePasse[100];
+
+	const char* m_ssid_pointAcces = "StationMeteo1234";
+	const char* m_motDePasse_pointAcces = "allo1234";
+
+	const char* m_serveur_mqtt = "192.168.2.52";
+    const int m_port_mqtt = 1883;
+    const char* m_utilisateur = "homeassistant";
+    const char* m_motDePasseUtilisateur = "ahweeNg0heeyi5aeX1geegh7eip7oht8Biebo6icilu9shee0gai8kieCho5awie";
+
+    unsigned long m_tempsDernierMessage;
+
+    WiFiClient m_espClient;
+    byte m_addr = 0x76;
+    PubSubClient m_client = PubSubClient(m_espClient);
+    Bme280 m_bme = Bme280(m_addr);
+
+    void Reconnecter();
+    void Publier();
+public:
+    ServeurMQTT();
+    void Configuration();
+    void Loop();
+};
---
+++ Station Meteo/src/ServeurMQTT.cpp
@@ -0,0 +1,122 @@
+#include <ServeurMQTT.h>
+#include <bme280.h>
+bool shouldSaveConfig = false;
+
+ServeurMQTT::ServeurMQTT() {}
+
+void saveConfigCallback()
+{
+    Serial.println("Devrait sauvegarder la config");
+    shouldSaveConfig = true;
+}
+
+void ServeurMQTT::Configuration()
+{
+    // WiFi.mode(WIFI_STA);
+    // this->m_wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+    // WiFiManagerParameter custom_mqtt_serveur("serveur", "mqtt serveur", this->mqtt_serveur, 40);
+    // WiFiManagerParameter custom_mqtt_port("port", "mqtt port", this->mqtt_port, 5);
+    // WiFiManagerParameter custom_mqtt_utitilisateur("utilisateur", "mqtt utilisateur", this->mqtt_utilisateur, 30);
+    // WiFiManagerParameter custom_mqtt_motDePasse("motDePasse", "mqtt motDePasse", this->mqtt_motDePasse, 100);
+
+    // this->m_wifiManager.addParameter(&custom_mqtt_serveur);
+    // this->m_wifiManager.addParameter(&custom_mqtt_port);
+    // this->m_wifiManager.addParameter(&custom_mqtt_utitilisateur);
+    // this->m_wifiManager.addParameter(&custom_mqtt_motDePasse);
+
+    //this->m_wifiManager.resetSettings();
+    //this->m_ssid_pointAcces, this->m_motDePasse_pointAcces
+
+    if (!this->m_wifiManager.autoConnect())
+    {
+        Serial.println("Erreur de connexion");
+        delay(2000);
+        ESP.restart();
+    }
+    else
+    {
+        Serial.println("Connexion etablie");
+    }
+
+    // strcpy(this->mqtt_serveur, custom_mqtt_serveur.getValue());
+    // strcpy(this->mqtt_port, custom_mqtt_port.getValue());
+    // strcpy(this->mqtt_utilisateur, custom_mqtt_utitilisateur.getValue());
+    // strcpy(this->mqtt_motDePasse, custom_mqtt_motDePasse.getValue());
+
+    this->m_client.setServer(this->m_serveur_mqtt, this->m_port_mqtt);
+}
+
+void ServeurMQTT::Reconnecter()
+{
+    while (!this->m_client.connected())
+    {
+        Serial.println("Connection au serveur Mqtt...");
+
+        if (this->m_client.connect("esp32", this->m_utilisateur, this->m_motDePasseUtilisateur))
+        {
+            Serial.println("Connecter");
+        }
+        else
+        {
+            Serial.print("Echec : ");
+            Serial.print(this->m_client.state());
+            Serial.println(" Reessayer dans 5 secondes");
+            delay(5000);
+        }
+    }
+}
+
+void ServeurMQTT::Publier()
+{
+    this->m_bme.LireInformationBme280();
+
+    float temperature = this->m_bme.EnvoyerTemperature();
+    float humidite = this->m_bme.EnvoyerHumidite();
+    float pression = this->m_bme.EnvoyerPression() / 100.0f;
+    float altitude = this->m_bme.EnvoyerAltitude();
+
+    Serial.println();
+
+    char temperatureString[15];
+    dtostrf(temperature, 7, 3, temperatureString);
+    Serial.print("Temperature: ");
+    Serial.println(temperatureString);
+    this->m_client.publish("esp32/temperature", temperatureString);
+
+    char humiditeString[15];
+    dtostrf(humidite, 7, 3, humiditeString);
+    Serial.print("Humidite: ");
+    Serial.println(humiditeString);
+    this->m_client.publish("esp32/humidite", humiditeString);
+
+    char pressionString[15];
+    dtostrf(pression, 7, 3, pressionString);
+    Serial.print("Pression: ");
+    Serial.println(pressionString);
+    this->m_client.publish("esp32/pression", pressionString);
+
+    char altitudeString[15];
+    dtostrf(altitude, 7, 3, altitudeString);
+    Serial.print("Altitude: ");
+    Serial.println(altitudeString);
+    this->m_client.publish("esp32/altitude", altitudeString);
+}
+
+void ServeurMQTT::Loop()
+{
+    if (!this->m_client.connected())
+    {
+        Reconnecter();
+    }
+    this->m_client.loop();
+
+    long temps = millis();
+
+    if (temps - this->m_tempsDernierMessage > 5000)
+    {
+        this->m_tempsDernierMessage = temps;
+
+        Publier();
+    }
+}
